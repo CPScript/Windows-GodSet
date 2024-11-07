a5ddabce54ecff63c6be8ae5751e18bb0e93ad33
Disease <83523587+CPScript@users.noreply.github.com>
2024-11-07 12:13:07 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,8 +4,8 @@ set "desktopPath=%USERPROFILE%\Desktop"
 set "godModePath=%desktopPath%\%folderName%"
 
 if exist "%godModePath%" (
-    echo The God Mode folder already exists.
-    set /p openChoice="Do you want to open the existing God Mode folder? (Y/N): "
+    echo The folder already exists.
+    set /p openChoice="Do you want to open the existing system file? (Y/N): "
     if /I "%openChoice%"=="Y" (
         start "" "%godModePath%"
     ) else (
@@ -14,12 +14,12 @@ if exist "%godModePath%" (
     exit /b
 )
 
-set /p createChoice="The God Mode folder does not exist. Do you want to create it? (Y/N): "
+set /p createChoice="The folder does not exist. Do you want to create it? (Y/N): "
 if /I "%createChoice%"=="Y" (
     mkdir "%godModePath%"
     echo God Mode folder created successfully.
     
-    set /p openNewChoice="Do you want to open the newly created God Mode folder? (Y/N): "
+    set /p openNewChoice="Do you want to open the newly created system file? (Y/N): "
     if /I "%openNewChoice%"=="Y" (
         start "" "%godModePath%"
     ) else (
